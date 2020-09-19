23a31f2dfe38f80851b90b4c39eef3961b5c50ed
shironekoa3 <shironekoa3@qq.com>
2020-09-19 16:16:27 +0800

    init

---
+++ src/main/java/store/longyan/controller/CategoryController.java
@@ -0,0 +1,28 @@
+package store.longyan.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import store.longyan.service.CategoryService;
+import store.longyan.service.TagService;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping(value = "/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public HashMap<String,Object> list(){
+
+        HashMap<String,Object> result = new HashMap<>();
+        result.put("status", 200);
+        result.put("data", categoryService.findAll());
+
+        return result;
+    }
+}
