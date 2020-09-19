02de56a3371642abe8de95824b6536179de21c3f
shironekoa3 <shironekoa3@qq.com>
2020-09-19 17:32:44 +0800

    init

--- src/main/java/store/longyan/test/TestMain.java
+++ src/main/java/store/longyan/test/TestMain.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import store.longyan.domain.Category;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
