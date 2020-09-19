d0c01b5a9f9f7373ac8a5443c0c4fba7b6e5c147
shironekoa3 <shironekoa3@qq.com>
2020-09-19 17:23:01 +0800

    init

--- src/main/java/store/longyan/controller/CategoryController.java
+++ src/main/java/store/longyan/controller/CategoryController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import store.longyan.domain.Category;
 import store.longyan.service.CategoryService;
-import store.longyan.service.TagService;
 
 import java.util.HashMap;
 
